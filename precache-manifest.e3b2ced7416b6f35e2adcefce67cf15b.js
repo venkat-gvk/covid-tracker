4095b2923247b43cb6912fb0115c372baebf3b66
krishna <krishna@pl.com>
2020-09-05 13:03:15 +0530

    Updates

---
+++ precache-manifest.e3b2ced7416b6f35e2adcefce67cf15b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a75336aa22e5c036620d546cde3b914a",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "7458f2dc32b3a902a7f5",
+    "url": "/covid-tracker/static/css/main.906edc54.chunk.css"
+  },
+  {
+    "revision": "5b85c3a14f9bee626a16",
+    "url": "/covid-tracker/static/js/2.0508a4bd.chunk.js"
+  },
+  {
+    "revision": "c46ade285a7bd9e73d4de71c9509bd70",
+    "url": "/covid-tracker/static/js/2.0508a4bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7458f2dc32b3a902a7f5",
+    "url": "/covid-tracker/static/js/main.f26d208d.chunk.js"
+  },
+  {
+    "revision": "1af475054be274cc1e5c",
+    "url": "/covid-tracker/static/js/runtime-main.9cfae146.js"
+  }
+]);
